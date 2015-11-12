ed9196b252906f9baf7d674a7f13cdf4b4f42992
marekpridal <m.pridal29@gmail.com>
2015-11-12 17:25:28 +0100

    Přidáno upozornění o ukládání cookies
    Přidáno upozornění o ukládání cookies

--- index.php
+++ index.php
@@ -15,6 +15,7 @@ include 'copyright.php';
     <link rel="stylesheet" href="styl.css" type="text/css">
     <link href="favicon.ico" rel="icon" type="image/png" />
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/1.0.9/cookieconsent.min.js"></script>
     </head>
     
 <body>
@@ -133,6 +134,10 @@ include 'copyright.php';
    copyright();
    ?>
   </footer>
+    
+<script type="text/javascript">
+    window.cookieconsent_options = {"message":"Tato stránka ukládá cookies a Evropská unie nařizuje tohle upozornění. Stačí odkliknout a jedeme dál.","dismiss":"Chápu...","learnMore":"Více info z české legislativy","link":"https://www.uoou.cz/vismo/zobraz_dok.asp?id_org=200144&id_ktg=1853&n=cookies-prechod-z-principu-opt-out-na-opt-in&query=cookie","theme":"dark-floating"};
+  </script>
   
 </body>
 </html>
