acdc669d3ae9952ee94e6026477ffb3cb411eb1a
Marek Pridal <m.pridal29@gmail.com>
2016-12-29 10:37:13 +0100

    oprava nemožnosti odeslat email z mobilu

--- index.php
+++ index.php
@@ -16,25 +16,25 @@ include 'copyright.php';
     <link rel="stylesheet" href="styl.css" type="text/css">
     <!--ikona-->
     <link rel="apple-touch-icon" sizes="57x57" href="/icons/apple-touch-icon-57x57.png">
-<link rel="apple-touch-icon" sizes="60x60" href="/icons/apple-touch-icon-60x60.png">
-<link rel="apple-touch-icon" sizes="72x72" href="/icons/apple-touch-icon-72x72.png">
-<link rel="apple-touch-icon" sizes="76x76" href="/icons/apple-touch-icon-76x76.png">
-<link rel="apple-touch-icon" sizes="114x114" href="/icons/apple-touch-icon-114x114.png">
-<link rel="apple-touch-icon" sizes="120x120" href="/icons/apple-touch-icon-120x120.png">
-<link rel="apple-touch-icon" sizes="144x144" href="/icons/apple-touch-icon-144x144.png">
-<link rel="apple-touch-icon" sizes="152x152" href="/icons/apple-touch-icon-152x152.png">
-<link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon-180x180.png">
-<link rel="icon" type="image/png" href="/icons/favicon-32x32.png" sizes="32x32">
-<link rel="icon" type="image/png" href="/icons/favicon-194x194.png" sizes="194x194">
-<link rel="icon" type="image/png" href="/icons/favicon-96x96.png" sizes="96x96">
-<link rel="icon" type="image/png" href="/icons/android-chrome-192x192.png" sizes="192x192">
-<link rel="icon" type="image/png" href="/icons/favicon-16x16.png" sizes="16x16">
-<link rel="manifest" href="/icons/manifest.json">
-<link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#17a2c9">
-<link rel="shortcut icon" href="/icons/favicon.ico">
-<meta name="msapplication-TileColor" content="#ffffff">
-<meta name="msapplication-TileImage" content="/icons/mstile-144x144.png">
-<meta name="msapplication-config" content="/icons/browserconfig.xml">
+    <link rel="apple-touch-icon" sizes="60x60" href="/icons/apple-touch-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/icons/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/icons/apple-touch-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/icons/apple-touch-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/icons/apple-touch-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/icons/apple-touch-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/icons/apple-touch-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon-180x180.png">
+    <link rel="icon" type="image/png" href="/icons/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="/icons/favicon-194x194.png" sizes="194x194">
+    <link rel="icon" type="image/png" href="/icons/favicon-96x96.png" sizes="96x96">
+    <link rel="icon" type="image/png" href="/icons/android-chrome-192x192.png" sizes="192x192">
+    <link rel="icon" type="image/png" href="/icons/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="/icons/manifest.json">
+    <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#17a2c9">
+    <link rel="shortcut icon" href="/icons/favicon.ico">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/icons/mstile-144x144.png">
+    <meta name="msapplication-config" content="/icons/browserconfig.xml">
 <!--konec inkludace ikon-->
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/1.0.9/cookieconsent.min.js"></script>
@@ -91,50 +91,50 @@ include 'copyright.php';
     
         <br>
         
-    <div id="second">
+    <div>
     <div class="row">
-    <div class="col-md-6 col-xs-12 col-sm-6">
-<form class="form-horizontal" method="post" action="index.php">
-    <div class="form-group">
-        <label for="name" class="col-sm-5 col-xs-12 control-label">Jméno</label>
-        <div class="col-md-7 col-xs-12 col-sm-6">
-            <input type="text" class="form-control" id="name" name="name" placeholder="Jméno a přijmení" value="<?php echo htmlspecialchars($_POST['name']); ?>">
-            <?php echo "<p class='text-danger'>$errName</p>";?>
-        </div>
-    </div>
-    <div class="form-group">
-        <label for="email" class="col-sm-5 col-xs-12 control-label">E-mail</label>
-        <div class="col-md-7 col-xs-12 col-sm-6">
-            <input type="email" class="form-control" id="email" name="email" placeholder="jan.novak@seznam.cz" value="<?php echo htmlspecialchars($_POST['email']); ?>">
-            <?php echo "<p class='text-danger'>$errEmail</p>";?>
-        </div>
-    </div>
-    <div class="form-group">
-        <label for="message" class="col-sm-5 col-xs-12 control-label">Zpráva</label>
-        <div class="col-md-7 col-xs-12 col-sm-6">
-            <textarea class="form-control" rows="4" name="message" placeholder="Váš vzkaz"><?php echo htmlspecialchars($_POST['message']);?></textarea>
-            <?php echo "<p class='text-danger'>$errMessage</p>";?>
-        </div>
-    </div>
-    <div class="form-group">
-        <label for="human" class="col-sm-5 col-xs-12 control-label">2 + 3</label>
-        <div class="col-md-7 col-xs-12 col-sm-6">
-            <input type="number" class="form-control" id="human" name="human" placeholder="Výsledek">
-            <?php echo "<p class='text-danger'>$errHuman</p>";?>
-        </div>
-    </div>
-    <div class="form-group">
-        <div class="col-md-7 col-xs-12 col-sm-6 col-sm-offset-5">
-            <input id="submit" name="submit" type="submit" value="Odeslat" class="btn btn-primary">
-        </div>
-    </div>
-    <div class="form-group">
-        <div class="col-md-7 col-xs-12 col-sm-6 col-sm-offset-5">
-            <?php echo $result; ?>
-            
-        </div>
-    </div>
-</form>
+    <div class="col-md-6">
+        <form class="form-horizontal" method="post" action="index.php">
+            <div class="form-group">
+                <label for="name" class="col-sm-5 control-label">Jméno</label>
+                <div class="col-md-7 col-sm-6">
+                    <input type="text" class="form-control" id="name" name="name" placeholder="Jméno a přijmení" value="<?php echo htmlspecialchars($_POST['name']); ?>">
+                    <?php echo "<p class='text-danger'>$errName</p>";?>
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="email" class="col-sm-5 control-label">E-mail</label>
+                <div class="col-md-7 col-sm-6">
+                    <input type="email" class="form-control" id="email" name="email" placeholder="jan.novak@seznam.cz" value="<?php echo htmlspecialchars($_POST['email']); ?>">
+                    <?php echo "<p class='text-danger'>$errEmail</p>";?>
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="message" class="col-sm-5 control-label">Zpráva</label>
+                <div class="col-md-7 col-sm-6">
+                    <textarea class="form-control" rows="4" name="message" placeholder="Váš vzkaz"><?php echo htmlspecialchars($_POST['message']);?></textarea>
+                    <?php echo "<p class='text-danger'>$errMessage</p>";?>
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="human" class="col-sm-5 control-label">2 + 3</label>
+                <div class="col-md-7 col-sm-6">
+                    <input type="number" class="form-control" id="human" name="human" placeholder="Výsledek">
+                    <?php echo "<p class='text-danger'>$errHuman</p>";?>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-md-7 col-sm-6 col-sm-offset-5">
+                    <input id="submit" name="submit" type="submit" value="Odeslat" class="btn btn-primary">
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-md-7 col-sm-6 col-sm-offset-5">
+                    <?php echo $result; ?>
+
+                </div>
+            </div>
+        </form>
         </div>  <!--ukončení col-md-6 pro formulář-->
         
         <div class="col-md-6">
@@ -165,10 +165,10 @@ include 'copyright.php';
            ?>
         </div>
   </footer>
-    
+  
 <script type="text/javascript">
     window.cookieconsent_options = {"message":"Tato stránka ukládá cookies a Evropská unie nařizuje tohle upozornění. Stačí odkliknout a jedeme dál.","dismiss":"Chápu...","learnMore":"Více info z české legislativy","link":"https://www.uoou.cz/vismo/zobraz_dok.asp?id_org=200144&id_ktg=1853&n=cookies-prechod-z-principu-opt-out-na-opt-in&query=cookie","theme":"dark-floating"};
   </script>
-  
+
 </body>
 </html>
