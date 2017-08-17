b6acecbc832c9b62d8dc41373bccd4519bdfec96
Marek Pridal <m.pridal29@gmail.com>
2017-08-17 16:02:22 +0200

    oprava komentáře

--- index.php
+++ index.php
@@ -35,7 +35,7 @@ include 'copyright.php';
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="msapplication-TileImage" content="/icons/mstile-144x144.png">
     <meta name="msapplication-config" content="/icons/browserconfig.xml">
-<!--konec inkludace ikon-->
+<!--konec include ikon-->
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/1.0.9/cookieconsent.min.js"></script>
     </head>
