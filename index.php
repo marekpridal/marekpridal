b40fc8758246d5404e1f9bc897a148f54a6bee82
Marek Pridal <m.pridal29@gmail.com>
2017-08-17 16:02:32 +0200

    změna procent u Swiftu

--- index.php
+++ index.php
@@ -60,8 +60,8 @@ include 'copyright.php';
 
             <h3 class="nadpis">Swift</h3>
             <div class="progress">
-                <div class="progress-bar progress-bar-info progress-bar-striped active" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%">
-                    <span class="graf-text">75%</span>
+                <div class="progress-bar progress-bar-info progress-bar-striped active" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
+                    <span class="graf-text">80%</span>
                 </div>
             </div>
 
